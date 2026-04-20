9c76a167bbe61089edd066987fc3c2016778767a
Joso1207 <joso1207@student.miun.se>
2026-04-20 10:39:59 +0200

    Todo1: JwtUtil
    GenerateToken(Username)
    validateToken(token)
    extractUsername(token)

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -1,11 +1,64 @@
 
 package com.example.demo.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.util.Date;
+
 public class JwtUtil {
 
+
     // TODO: Implementera:
-    // - generateToken(username)
-    // - validateToken(token)
-    // - extractUsername(token)
+    // - generateToken(username) ☑
+    // - validateToken(token) ☑
+    // - extractUsername(token)  ☑
+
+
+    private final String SECRET = "mysecretkeymysecretkeymysecretkey";
+
+    public String generateToken(String username){
+        try{
+            return Jwts.builder()
+                    .setSubject(username)
+                    .setIssuedAt(new Date())
+                    .setExpiration(new Date(
+                            System.currentTimeMillis()+1800000
+                    ))
+                    .signWith(SignatureAlgorithm.HS256,SECRET.getBytes())
+                    .compact();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            throw e;
+        }
+
+    }
+
+    public String validateToken(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(SECRET.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
+
+
 
 }
