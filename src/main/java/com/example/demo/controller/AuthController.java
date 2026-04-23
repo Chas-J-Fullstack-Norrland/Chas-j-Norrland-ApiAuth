56b64d4345bc25a0cb9487f88fe70e78f2e1dc2e
Joso1207 <joso1207@student.miun.se>
2026-04-23 12:31:36 +0200

    For code clarity,  Showing what Authentication Manager does under the hood, while showing we dont need its authentication objects.

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,36 +1,55 @@
 
 package com.example.demo.controller;
-import com.example.demo.model.LoginRequestDTO;
+import com.example.demo.model.AppUser;
+import com.example.demo.repo.UserRepository;
 import com.example.demo.security.JwtUtil;
 import org.springframework.security.authentication.*;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AuthController {
-    private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
-    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
-        this.authenticationManager = authenticationManager;
+    private final UserRepository repo;
+    private final BCryptPasswordEncoder encoder;
+    public AuthController(UserRepository repo, JwtUtil jwtUtil, BCryptPasswordEncoder encoder) {
         this.jwtUtil = jwtUtil;
+        this.repo = repo;
+        this.encoder = encoder;
+    }
+
+    //Needed since we added in password encryption while the assignment assumed none
+    @PostMapping("/register")
+    public String register(@RequestParam String username, @RequestParam String password){
+
+        AppUser appUser = new AppUser();
+        appUser.setUsername(username);
+        appUser.setPassword(encoder.encode(password));
+        try{
+            AppUser newAppUser = repo.save(appUser);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return "Failed";
+        }
+
+
+        return "User: " + appUser.getUsername() + " was created";
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequestDTO requestDTO) {
-        //1. Försök autentisera via Spring Security
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        requestDTO.getUsername(),
-                        requestDTO.getPassword()
-                )
-        );
+    public String login(@RequestParam String username, @RequestParam String password) {
+
+        //authenticationManager.authenticate( new UsernamePasswordAuthenticationToken()
+        //does the same thing as the next 4 lines but returns the authentication we are supposed to arrive to with JWT
+        AppUser appUser = repo.findByUsername(username).orElseThrow(()-> new BadCredentialsException("Username and Password didnt match"));
+        if(!encoder.matches(password, appUser.getPassword())){
+            throw new BadCredentialsException("Username or Password Didn't match");
+        }
         //2. Om vi kommer hit = Login OK
-        String username = authentication.getName();
+        String name = appUser.getUsername();
         //3. Skapa JWT
-        String token = jwtUtil.generateToken(username);
         //4. Returnera token
-        return token;
+        return jwtUtil.generateToken(name);
 
     }
 }
