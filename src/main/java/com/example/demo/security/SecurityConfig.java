adf86a54d0d4e2ab689288e16f343f96194f940a
Joso1207 <joso1207@student.miun.se>
2026-04-28 11:58:31 +0200

    Making the message be shown in browser

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -34,10 +34,10 @@ public class SecurityConfig {
                 )
                 .exceptionHandling(ex -> ex
                         .authenticationEntryPoint((req, res, e) -> {
-                                    res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-                                    res.getWriter().write("Unauthorized, please log in via /login or /oauth2/authorization/github");
-                                }
-                        )
+                            res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            res.setContentType("text/plain");
+                            res.getWriter().write("Unauthorized, please log in via /login or /oauth2/authorization/github");
+                        })
                 )
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
             // TODO:
